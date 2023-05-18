29b8e926a9cff93b2a7a4af8b6406e4025c6ebd6
noy <noyhason3@gmail.com>
2023-05-18 23:15:51 +0300

    icons added

--- src/cmps/footer/Footer.tsx
+++ src/cmps/footer/Footer.tsx
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Footer.scss'
+import { faBarsStaggered, faComment, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export const Footer = () => {
     return <section className="footer">
-        <div className="footer-tools-personal">P</div>
-        <div className="footer-tools-slides">S</div>
-        <div className="footer-tools-likes">L</div>
-        <div className="footer-tools-chats">C</div>
+        <div className="footer-tools-personal"><FontAwesomeIcon icon={faUser} /></div>
+        <div className="footer-tools-slides"><FontAwesomeIcon icon={faBarsStaggered} fontSize={'18px'} /></div>
+        <div className="footer-tools-likes"><FontAwesomeIcon icon={faHeart} /></div>
+        {/* <div className="footer-tools-likes"><FontAwesomeIcon icon={faHeart} style={{ color: "#a2a8b3" }} /></div> */}
+        <div className="footer-tools-chats"><FontAwesomeIcon icon={faComment} /></div>
     </section>
 }
